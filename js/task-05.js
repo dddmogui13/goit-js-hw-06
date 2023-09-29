1d97bedd956fca69c166fc0ecfcbe51e56344ef6
dddmogui13 <dmytropotorocha213@gmail.com>
2023-09-29 21:22:33 +0300

    upd

---
+++ js/task-05.js
@@ -0,0 +1,11 @@
+const nameInput = document.querySelector('#name-input');
+const nameOutput = document.querySelector('#name-output');
+ 
+nameInput.addEventListener('input', function () { 
+  const inputValue = nameInput.value;
+  if (inputValue === '') {
+    nameOutput.textContent = 'Anonymous';
+  } else {
+    nameOutput.textContent = inputValue;
+  }
+});
