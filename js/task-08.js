c6a5c9a98a7e7459bd8c5dbf8030e5770a8cf8be
dddmogui13 <dmytropotorocha213@gmail.com>
2023-09-29 21:32:18 +0300

    upd

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const loginForm = document.querySelector(".login-form");
+const emailInput = loginForm.elements.email;
+const passwordInput = loginForm.elements.password;
 
+loginForm.addEventListener("submit", function (event) {
+  event.preventDefault(); 
+
+  if (emailInput.value === "" || passwordInput.value === "") {
+    alert("Всі поля повинні бути заповнені!");
+  } else {
+
+    const formData = {
+      email: emailInput.value,
+      password: passwordInput.value,
+    };
+
+    console.log(formData);
+
+    loginForm.reset();
+  }
+});
